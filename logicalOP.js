343fac0d3c4a02f86eed68d1b0be53a5507ed550
Zubaer16 <ahmed.zubaer@northsouth.edu>
2021-04-13 18:27:02 +0600

    switch ::: 9:22

--- logicalOP.js
+++ logicalOP.js
@@ -16,4 +16,12 @@
 
 
 
-var letter 
+var letter = prompt("Enter a letter :");
+
+letter = letter.toLowerCase();
+
+if (letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u" )
+    console.log("vowel");
+
+else
+    console.log("consonant");
