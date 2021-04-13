db5f899f7172053463cd9cf72d5f740a8b84bcb6
Zubaer16 <ahmed.zubaer@northsouth.edu>
2021-04-13 12:48:36 +0600

    shape

---
+++ keywordDatatypeComment.js
@@ -0,0 +1,3 @@
+/*document.write("Anisul Islam <br/>");
+document.write("0000000000000 <br/>");
+document.write("Anisul Islam");*/
---
+++ numberMethod.js
@@ -0,0 +1,15 @@
+/*var num = "20.5";
+
+num  = parseFloat(num);
+
+// num = toString(num);
+
+console.log(typeof(num));*/
+
+// var number = 2.4678;
+
+// console.log(number.toPrecision(3))
+
+
+console.log(typeof  (Number("12")))
+console.log(Number(false))
