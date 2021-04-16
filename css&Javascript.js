deb92c0aa219f895cf2bd93eed3df1a3e2d6e040
Zubaer16 <ahmed.zubaer@northsouth.edu>
2021-04-16 16:44:49 +0600

    audio play

---
+++ css&Javascript.js
@@ -0,0 +1,15 @@
+var myVar = document.querySelector("#paraid");
+function addstyle(){
+
+    
+    myVar.classList.add("para-style");   
+}
+
+function removestyle(){
+
+    
+    myVar.classList.remove("para-style");
+
+
+
+}
---
+++ eventListener.js
@@ -0,0 +1,24 @@
+// document.querySelector("button").addEventListener("click",function(){
+//     alert("Hello");
+// });
+
+
+
+// function myFunction(){
+
+//     alert("Hello");
+// }
+
+
+var myVar = document.querySelector("h1");
+
+myVar.addEventListener("mouseover",function(){
+
+    myVar.classList.add("my-style");
+});
+
+
+myVar.addEventListener("mouseout",function(){
+
+    myVar.classList.remove("my-style");
+});
