5c36eec363ea83ec6edc86565454c2b2c86ece1a
Zubaer16 <ahmed.zubaer@northsouth.edu>
2021-04-25 15:32:04 +0600

    end

--- promisePart3.js
+++ promisePart3.js
@@ -26,13 +26,22 @@ const taskFour = () =>{
         
     };
 
-taskOne()
-.then((res) => console.log(res))
-.then(taskTwo)
-.then((res) => console.log(res))
-.then(taskThree)
-.then((res) => console.log(res))
-.then(taskFour)
-.then((res) => console.log(res))
-.catch(err => console.log(err)); 
+
+const callAllTasks = async() =>{
+    try {
+        const t1 = await taskOne();
+        console.log(t1);
+        const t2 = await taskTwo();
+        console.log(t2);
+        const t3 = await taskThree();
+        console.log(t3);
+        const t4 = await taskFour();
+        console.log(t4);
+    }catch (e){
+        console.log(e);
+    }
+}
+
+callAllTasks();
+
 console.log("bye");
